5d3b030ec03fecd004e6c232a82fca6279479444
lODIN007l <wilsonlluilema007@gmail.com>
2022-06-15 01:59:31 -0500

    class Banda

---
+++ models/bandas.js
@@ -0,0 +1,34 @@
+
+
+
+class Bandas{
+    constructor(){
+        this.bandas=[]
+
+    }
+    addBanda(bandObj= new Banda()){
+        this.bandas.push(bandObj);
+    }
+    getBandas(){
+        return this.bandas
+    }
+
+    deleteBanda(id=''){
+        this.bandas=this.bandas.filter(bandObj=>bandObj.id!==id);
+        return this.bandas
+    }
+
+    voteBandas(id=''){
+        this.bandas+this.bandas.map(bandObj=>{
+            if(bandObj.id===id){
+                bandObj.votes++;
+                return bandObj;
+            }else{
+                return bandObj;
+            }
+        })
+    }
+
+}
+
+module.exports=Bandas;
--- sockets/socket.js
+++ sockets/socket.js
@@ -6,13 +6,21 @@ io.on('connection',client =>{
 
 
     client.on('disconnect',()=>{
-        console.log('Cliente conectado');
+        console.log('Cliente desconectado');
     })
 
     client.on('mensaje',(payload)=>{
         console.log('Mensaje',payload);
         io.emit('mensaje',{admin:'nuevo mensaje'})
     })
+
+    client.on('emitir-mensaje',(payload)=>{
+        //console.log(payload)
+        //io.emit('nuevo-mensaje',payload) //emite a todos incluido el que envio
+        client.broadcast.emit('nuevo-mensaje',payload)
+    })
+   
+
 
 });
 
