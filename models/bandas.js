cd335ac3fb20a85926236fc3e4eff20b04457cb1
lODIN007l <wilsonlluilema007@gmail.com>
2022-06-16 19:05:33 -0500

    logica de bandas

--- models/bandas.js
+++ models/bandas.js
@@ -1,4 +1,4 @@
-
+const Banda =require('./banda');
 
 
 class Bandas{
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,27 @@
 const {io} = require('../index');
+const Bandas=require('../models/bandas')
+const Banda=require('../models/banda')
+
+
+const bandass= new Bandas();
+bandass.addBanda(new Banda('Linkin Park'));
+bandass.addBanda(new Banda('Metalica'));
+bandass.addBanda(new Banda('Queen'));
+bandass.addBanda(new Banda('bon jovi'));
+bandass.addBanda(new Banda('punk'));
+
+
+//console.log(bandass);
+
+
+
+
+
 
 //Socket messaje 
 io.on('connection',client =>{
     console.log('Cliente conectado');
-
+    client.emit('bandas-registradas',bandass.getBandas());
 
     client.on('disconnect',()=>{
         console.log('Cliente desconectado');
@@ -14,11 +32,34 @@ io.on('connection',client =>{
         io.emit('mensaje',{admin:'nuevo mensaje'})
     })
 
-    client.on('emitir-mensaje',(payload)=>{
-        //console.log(payload)
-        //io.emit('nuevo-mensaje',payload) //emite a todos incluido el que envio
-        client.broadcast.emit('nuevo-mensaje',payload)
+    // client.on('emitir-mensaje',(payload)=>{
+    //     //console.log(payload)
+    //     //io.emit('nuevo-mensaje',payload) //emite a todos incluido el que envio
+    //     client.broadcast.emit('nuevo-mensaje',payload)
+    // })
+    
+    //votos de bandas
+    client.on('votar-banda',(payload)=>{
+       //console.log(payload)
+      bandass.voteBandas(payload.id);
+      //enviamos con io para  se actualice a todos inclusive a uien mando 
+      io.emit('bandas-registradas',bandass.getBandas());
     })
+    //agregar bandas 
+    client.on('nueva-banda',(payload)=>{
+        console.log(payload)
+        bandass.addBanda(new Banda(payload.name))
+    //    //enviamos con io para  se actualice a todos inclusive a uien mando 
+        io.emit('bandas-registradas',bandass.getBandas());
+     });
+     //eliminar banda 
+     client.on('eliminar-banda',(payload)=>{
+        //console.log(payload)
+       bandass.deleteBanda(payload.id);
+       //enviamos con io para  se actualice a todos inclusive a uien mando 
+       io.emit('bandas-registradas',bandass.getBandas());
+     })
+
    
 
 
